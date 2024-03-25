8f6cc6d573126b0d8c97081bf83250acc21bc0f3
Jose Ibanez <jocibanez1904@gmail.com>
2024-03-24 22:16:35 -0500

    Cambia los enlaces de imágenes y agrega comentarios al main.js

--- main.js
+++ main.js
@@ -1,36 +1,35 @@
-function enlace (url) {
-    window.location.href = url   
+// Función para redireccionar a una URL específica
+function enlace(url) {
+    window.location.href = url;
 }
 
-function ActualizarHora () {
+// Función para actualizar la hora en tiempo real
+function ActualizarHora() {
+    // Obtener la fecha y hora actual
     let fecha = new Date();
+    // Obtener los segundos, minutos y horas actuales y formatearlos
     let segundos = fecha.getSeconds().toString().padStart(2, '0');
     let minutos = fecha.getMinutes().toString().padStart(2, '0');
     let horas = fecha.getHours().toString();
-    let ampm  
-    
+    let ampm;
+
+    // Obtener los elementos del DOM donde se mostrarán los datos de la hora
     let elementoHoras = document.getElementById("pHoras");
     let elementoMinutos = document.getElementById("pMinutos");
     let elementoSegundos = document.getElementById("pSegundos");
     let elementoAmPm = document.getElementById("am-pm");
 
-    horas = horas % 12
+    // Convertir las horas al formato de 12 horas y determinar si es a.m. o p.m.
+    horas = horas % 12;
     horas = horas ? horas : 12;
-    ampm = horas >= 12 ? " a.m" : " p.m"
+    ampm = horas >= 12 ? " a.m." : " p.m.";
 
+    // Actualizar los elementos del DOM con los nuevos valores de hora
     elementoHoras.textContent = horas;
     elementoMinutos.textContent = minutos;
     elementoSegundos.textContent = segundos;
     elementoAmPm.textContent = ampm;
-
-    // if (horas >= 8 && minutos >= 1 && horas < 12) {
-    //     pSaludo.textContent = "buenos días";
-    // }
-    // if (horas >= 12 && minutos >= 1 && horas < 19) {
-    //     pSaludo.textContent = "buenas tardes";
-    // }
-    // if (horas >= 19 && minutos >= 1) {
-    //     pSaludo.textContent = "buenas noches";
-    // }
 }
- setInterval(ActualizarHora,1000);
+
+// Llamar a la función ActualizarHora cada segundo
+setInterval(ActualizarHora, 1000);
